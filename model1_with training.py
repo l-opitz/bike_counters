abe239d87a29b72dfc8791cbdba1a68e784d236e
l-opitz <louisopitz15@gmail.com>
2024-12-09 22:57:39 +0100

    Model1 Script with training on Kaggle

---
+++ model1_with training.py
@@ -0,0 +1,120 @@
+#%%
+
+import pandas as pd
+from pathlib import Path
+from joblib import load
+import holidays
+import numpy as np
+import sklearn
+from sklearn.model_selection import TimeSeriesSplit
+from sklearn.preprocessing import FunctionTransformer, StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import make_pipeline
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+
+# Load train data
+data = pd.read_parquet("/kaggle/input/msdb-2024/train.parquet") # to load on Kaggle locally
+#data = pd.read_parquet(Path("data") / "train.parquet") # to load locally
+
+def _encode_dates(X):
+    X = X.copy()  # modify a copy of X
+    # Encode the date information from the DateOfDeparture columns
+    X["year"] = X["date"].dt.year
+    X["month"] = X["date"].dt.month
+    X["day"] = X["date"].dt.day
+    X["weekday"] = X["date"].dt.weekday
+    X["hour"] = X["date"].dt.hour
+
+    return X
+
+# Feature manipulation for train and test data
+def preprocess_data(data):
+    data = data.copy()
+
+    # Apply date encoding
+    data = _encode_dates(data)
+
+    # Add weekend column
+    data["weekend"] = (data["weekday"] > 4).astype(int)  # 1 stands for weekend, 0 stands for no weekend
+
+    # Add French holidays column
+    FR_holidays = holidays.FR(years=range(2019, 2022))
+    data["FR_holidays"] = data["date"].dt.date.isin(FR_holidays).astype(int)
+
+    return data
+
+# Manipulate train data
+train_data = preprocess_data(data)
+
+# Extract train and validate data
+def get_train_data(data = train_data, _target_column_name = "log_bike_count"):
+    # Sort by date first, so that time based cross-validation would produce correct results
+    data = data.sort_values(["date", "counter_name"])
+    y_array = data[_target_column_name].values
+    X_df = data.drop([_target_column_name, "bike_count"], axis=1)
+    return X_df, y_array
+
+X, y = get_train_data()
+
+def train_test_split_temporal(X, y, delta_threshold="30 days"):
+    
+    cutoff_date = X["date"].max() - pd.Timedelta(delta_threshold)
+    mask = (X["date"] <= cutoff_date)
+    X_train, X_valid = X.loc[mask], X.loc[~mask]
+    y_train, y_valid = y[mask], y[~mask]
+
+    return X_train, y_train, X_valid, y_valid
+
+X_train, y_train, X_valid, y_valid = train_test_split_temporal(X, y)
+
+# Training the model
+date_encoder = FunctionTransformer(_encode_dates)
+date_cols = _encode_dates(X_train[["date"]]).columns.tolist()
+date_cols = [col for col in date_cols if col != "day"] # exclude "day" in one hot encoding
+
+categorical_encoder = OneHotEncoder(handle_unknown="ignore")
+categorical_cols = ["counter_name", "site_name"]
+
+binary_cols = ["weekend", "FR_holidays"] # No transformation required, they are already binary
+
+
+preprocessor = ColumnTransformer(
+    [
+        ("date", OneHotEncoder(handle_unknown="ignore"), date_cols),
+        ("cat", categorical_encoder, categorical_cols),
+        ("binary", "passthrough", binary_cols)
+    ]
+)
+
+regressor = RandomForestRegressor(random_state=42, max_depth=36, n_estimators=7, n_jobs=-1)
+
+pipe = make_pipeline(date_encoder, preprocessor, regressor)
+pipe.fit(X_train, y_train)
+
+# Load the test data
+test_data = pd.read_parquet("/kaggle/input/msdb-2024/final_test.parquet") # to load on Kaggle
+#test_data = pd.read_parquet(Path("data") / "final_test.parquet") # to load locally
+
+test_data = preprocess_data(test_data)
+
+# Extract features (X) for prediction
+X_test = test_data
+
+# Predict on test data
+y_pred = pipe.predict(X_test)
+
+# Save predictions
+results = pd.DataFrame(
+    dict(
+        Id=np.arange(y_pred.shape[0]),
+        log_bike_count=y_pred,
+    )
+)
+output_path = "submission1.csv"
+results.to_csv(output_path, index=False)
+print(f"Predictions saved to {output_path}")
+
+
+print(f"Train set, RMSE={mean_squared_error(y_train, pipe.predict(X_train), squared=False):.2f}")
+print(f"Valid set, RMSE={mean_squared_error(y_valid, pipe.predict(X_valid), squared=False):.2f}")
